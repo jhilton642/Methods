9d54b73910e53287e3ad4d03898bf211441bbc28
jhilton642 <joshua.hilton@att.net>
2020-09-23 09:35:02 -0500

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //  The user will enter 
+                //      Y, M, or D to get the Year, Month or Day of the current date
+                //      h, m, or s to get the hour, minute, or second of the current time
+                string part = Utils.GetInput("What part of the date are you interested in? (M or D or Y for month day or year): ");
+
+                //  this method will take the part of the date requested and from the current date return the units for that date part or time part
+                int timeUnit = GetDatePart(part);
+                if (timeUnit == -1)
+                    break;
+                Console.WriteLine($"The time part represented by {part} is {timeUnit}");
+            }
+        }
+        static int GetDatePart(string part)
+        {
+           int numberDate = 0;
+            if(part == "Y")
+            {
+                numberDate = DateTime.Today.Year;
+            }
+            if(part == "M")
+            {
+                numberDate = DateTime.Today.Month;
+            }
+            if (part == "D")
+            {
+               numberDate = DateTime.Today.Day;
+            }
+            return numberDate;
+        }
+    }
+}
---
+++ Methods/Utils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Methods
+{
+    class Utils
+    {
+        public static string GetInput(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        public static float GetFloat(string prompt)
+        {
+            float num;
+            Console.Write(prompt);
+            do
+            {
+                if (float.TryParse(Console.ReadLine(), out num))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Bad input. Try again");
+                }
+            } while (true);
+            return num;
+        }
+
+        public static int GetNumber(string prompt)
+        {
+            int num;
+            Console.Write(prompt);
+            do
+            {
+                if (Int32.TryParse(Console.ReadLine(), out num))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Bad input. Try again");
+                }
+            } while (true);
+            return num;
+        }
+
+        public static int GetNumber(string prompt, int max)
+        {
+            int num;
+            do
+            {
+                num = GetNumber(prompt);
+                if (num > max)
+                {
+                    Console.WriteLine("Number too large");
+                }
+            } while (num > max);
+            return num;
+        }
+    }
+}
